917ce0cc46c8abe0e20d788bdc4ad9f8bb964627
Ali Can MOGOL <alican@acm-ThinkPad-SL410.(none)>
2013-03-05 00:54:06 +0200

    new session key added for authorization and authentication

--- src/com/fererlab/dto/SessionKeys.java
+++ src/com/fererlab/dto/SessionKeys.java
@@ -12,6 +12,7 @@ public enum SessionKeys {
     SESSION_ID("SESSION_ID"),
     IS_LOGGED("IS_LOGGED"),
     COOKIE_SIGN_KEY("fr_ck_sn_ky"),
+    GROUP_NAMES("GROUP_NAMES"),
     COOKIE("Cookie");
 
     private final String value;
